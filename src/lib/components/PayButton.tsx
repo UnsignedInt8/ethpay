f246594545dde2b351e237878f48930bf0f7a068
UnsignedInt8 <unsignedint8@gmail.com>
2020-01-23 17:49:23 +0800

    [fix] fix font size

--- src/lib/components/PayButton.tsx
+++ src/lib/components/PayButton.tsx
@@ -79,7 +79,7 @@ const PayButton = (props: Props) => {
             <div className='icon'><currency.svg /></div>
             <span>Pay</span>
 
-            <ReactTooltip globalEventOff='click' effect="solid" multiline={false} />
+            <ReactTooltip globalEventOff='click' effect="solid" className='__ethpay_tooltip' type='warning' multiline={false} />
         </button>
     );
 }
